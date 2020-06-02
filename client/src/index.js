64a7b708f844d87bc92401064c8aafdc628daf64
Sarah Carter <sarahmarie.carter@yahoo.com>
2020-06-02 11:00:18 -0700

    created simple dashboard, installed react-router-dom, and created simple router in app.js

--- client/src/index.js
+++ client/src/index.js
@@ -4,12 +4,29 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import { set, get } from 'idb-keyval';
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+async function testIDBKeyval() {
+  let contact = {
+    name: 'sarah',
+    festival: 'edc',
+    note: 'super cute girl',
+    image: 'http://...',
+    phone: '253-245-8544'
+  }
+  await set('sarah carter', contact);
+  const whatDoWeHave = await get('sarah carter');
+  console.log(`When we queried idb-keyval for 'hello', we found: ${whatDoWeHave}`);
+}
+
+testIDBKeyval();
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
---
+++ client/src/pages/dashboard/dashboard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Dashboard() {
+    return(
+        <div className='dash-buttons'>
+            <button>View Friends</button><br />
+            <button>Add A Friend</button>
+        </div>
+    )
+}
+
+export default Dashboard;
