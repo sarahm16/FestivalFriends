d0d0ae27b8a8ffde9b9407ce84cb3d23778d52b6
Sarah Carter <sarahmarie.carter@yahoo.com>
2020-06-02 11:55:33 -0700

    render new Contact component in contacts.js for each key in the database

--- client/src/pages/contacts/contacts.js
+++ client/src/pages/contacts/contacts.js
@@ -7,16 +7,29 @@ import './style.css';
 
 class Contacts extends Component {
 
+    constructor() {
+        super();
+        this.state = {
+            contacts: []
+        }
+    }
+
     componentDidMount() {
-        keys().then(keys => console.log(keys));
+        keys().then(keys => {
+            console.log(keys)
+            this.setState({contacts: keys})
+        });
     }
 
     //render new contact for each key from database
 
     render() {
+        console.log('this.state.contacts: ' + this.state.contacts);
         return(
             <div className='container'>
-                Welcome to the contacts page!
+                {this.state.contacts.map(contact => {
+                    return <Contact name={contact} />
+                })}
                 <Contact name='Sarah'/>
                 <Contact name='Jonathon' />
                 <Contact name='Ben' />
