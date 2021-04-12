93aee0aece66a8c9c92fc9ef9f8a5fcafdcc740c
Sarah Carter <sarahmarie.carter@yahoo.com>
2021-04-12 09:33:55 -0700

    installed compression npm package

--- server.js
+++ server.js
@@ -28,23 +28,28 @@ const express = require('express');
 const favicon = require('express-favicon');
 const path = require('path');
 const port = process.env.PORT || 8080;
+var compression = require('compression');
+
 const app = express();
+app.use(compression());
+
 const expressStaticGzip = require('express-static-gzip');
 
 app.use(favicon(__dirname + '/client/build/favicon.ico'));
+
 // the __dirname is the current directory from where the script is running
 app.use(express.static(__dirname));
 
-//app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(path.join(__dirname, 'client/build')));
 
-const buildPath = path.join(__dirname, '..', 'client/build');
-app.use(
-  '/',
-  expressStaticGzip(buildPath, {
-    enableBrotli: true,
-    orderPreference: ['br', 'gz']
-  })
-);
+// const buildPath = path.join(__dirname, '..', 'client/build');
+// app.use(
+//   '/',
+//   expressStaticGzip(buildPath, {
+//     enableBrotli: true,
+//     orderPreference: ['br', 'gz']
+//   })
+// );
 
 app.get('*', function (req, res) {
   res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
