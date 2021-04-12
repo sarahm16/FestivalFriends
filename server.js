201a50de2e893656b9dce11c47e9e761afc173a4
Sarah Carter <sarahmarie.carter@yahoo.com>
2021-04-11 20:57:09 -0700

    added express-favicon package. Changed start script in root package.json to node server.js. Changed app title in index.html

--- server.js
+++ server.js
@@ -1,21 +1,39 @@
+// const express = require('express');
+// const bodyParser = require('body-parser');
+// const path = require('path');
+// const app = express();
+// const port = process.env.PORT || 5000;
+
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
+
+
+// if (process.env.NODE_ENV === 'production') {
+//   // Serve any static files
+//   app.use(express.static(path.join(__dirname, 'client/build')));
+
+//   // Handle React routing, return all requests to React app
+//   app.get('*', function(req, res) {
+//     res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+//   });
+// }
+
+// app.listen(port, () => console.log(`Listening on port ${port}`));
+
+
 const express = require('express');
-const bodyParser = require('body-parser');
+const favicon = require('express-favicon');
 const path = require('path');
+const port = process.env.PORT || 8080;
 const app = express();
-const port = process.env.PORT || 5000;
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-
-if (process.env.NODE_ENV === 'production') {
-  // Serve any static files
-  app.use(express.static(path.join(__dirname, 'client/build')));
-
-  // Handle React routing, return all requests to React app
-  app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-  });
-}
-
+app.use(favicon(__dirname + '/build/favicon.ico'));
+// the __dirname is the current directory from where the script is running
+app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, 'build')));
+app.get('/ping', function (req, res) {
+  return res.send('pong');
+});
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 app.listen(port, () => console.log(`Listening on port ${port}`));
