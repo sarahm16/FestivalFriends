da4344eb8a229734aa911e02c4e11046715e15c8
Sarah Carter <sarahmarie.carter@yahoo.com>
2021-04-11 21:07:18 -0700

    changed route to build folder in server.js

--- server.js
+++ server.js
@@ -1,42 +1,42 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const path = require('path');
-const app = express();
-const port = process.env.PORT || 5000;
-const favicon = require('express-favicon');
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-app.use(favicon(__dirname + '/build/favicon.ico'));
-
-
-if (process.env.NODE_ENV === 'production') {
-  // Serve any static files
-  app.use(express.static(path.join(__dirname, 'client/build')));
-
-  // Handle React routing, return all requests to React app
-  app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-  });
-}
-
-app.listen(port, () => console.log(`Listening on port ${port}`));
-
-
 // const express = require('express');
-// const favicon = require('express-favicon');
+// const bodyParser = require('body-parser');
 // const path = require('path');
-// const port = process.env.PORT || 8080;
 // const app = express();
+// const port = process.env.PORT || 5000;
+// const favicon = require('express-favicon');
+
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
+
 // app.use(favicon(__dirname + '/build/favicon.ico'));
-// // the __dirname is the current directory from where the script is running
-// app.use(express.static(__dirname));
-// app.use(express.static(path.join(__dirname, 'build')));
-// app.get('/ping', function (req, res) {
-//   return res.send('pong');
-// });
-// app.get('*', function (req, res) {
-//   res.sendFile(path.join(__dirname, 'build', 'index.html'));
-// });
+
+// console.log(process.env.NODE_ENV);
+// if (process.env.NODE_ENV === 'production') {
+//   // Serve any static files
+//   app.use(express.static(path.join(__dirname, 'client/build')));
+
+//   // Handle React routing, return all requests to React app
+//   app.get('*', function(req, res) {
+//     res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+//   });
+// }
+
 // app.listen(port, () => console.log(`Listening on port ${port}`));
+
+
+const express = require('express');
+const favicon = require('express-favicon');
+const path = require('path');
+const port = process.env.PORT || 8080;
+const app = express();
+app.use(favicon(__dirname + '/client/build/favicon.ico'));
+// the __dirname is the current directory from where the script is running
+app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, 'client/build')));
+app.get('/ping', function (req, res) {
+  return res.send('pong');
+});
+app.get('*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+});
+app.listen(port, () => console.log(`Listening on port ${port}`));
