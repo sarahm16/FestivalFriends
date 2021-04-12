0c648c5de9c17033ea13c5544f6addfc2afb141b
Sarah Carter <sarahmarie.carter@yahoo.com>
2021-04-12 09:13:27 -0700

    added compress-create-react-app package. Created new build. Added express-static-gzip package to direct server to serve compressed files

--- server.js
+++ server.js
@@ -29,13 +29,23 @@ const favicon = require('express-favicon');
 const path = require('path');
 const port = process.env.PORT || 8080;
 const app = express();
+const expressStaticGzip = require('express-static-gzip');
+
 app.use(favicon(__dirname + '/client/build/favicon.ico'));
 // the __dirname is the current directory from where the script is running
 app.use(express.static(__dirname));
-app.use(express.static(path.join(__dirname, 'client/build')));
-app.get('/ping', function (req, res) {
-  return res.send('pong');
-});
+
+//app.use(express.static(path.join(__dirname, 'client/build')));
+
+const buildPath = path.join(__dirname, '..', 'client/build');
+app.use(
+  '/',
+  expressStaticGzip(buildPath, {
+    enableBrotli: true,
+    orderPreference: ['br', 'gz']
+  })
+);
+
 app.get('*', function (req, res) {
   res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
 });
